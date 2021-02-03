879303ba03c2b6b5ad64effcdf545d4bf87f698f
stella6767 <alsrb9434@gmail.com>
2021-02-03 11:00:43 +0900

    styled-componenet 확장

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,12 @@ import styled from 'styled-components';
 const StyledHeaButtoderDiv = styled.div`
   border: 1px solid black;
   height: 300px;
+  background-color: ${(props) => props.backgroundColor};
 `;
 
 const Header = () => {
   return (
-    <StyledHeaButtoderDiv>
+    <StyledHeaButtoderDiv backgroundColor={'blue'}>
       <ul>
         <li>메뉴 1</li>
         <li>메뉴 2</li>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,23 +3,29 @@ import styled from 'styled-components';
 
 // Function 방식
 // class 방식
+const StyledDeleteButton = styled.button`
+  color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
+`; //디자인 동적결정
+
+//스타일 확장
+const StyledAddButton = styled(StyledDeleteButton)`
+  color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
+  background-color: green;
+`; //디자인 동적결정
 
 //부모로부터 받아온 어떤 데이터를 가지고 동적으로 스타일링을 할 거라면.
 const Home = (props) => {
   // 구조분할 할당
   const { boards, setBoards, number, setNumber, user } = props;
-
-  const StyledDeleteButton = styled.button`
-    color: ${(props) => (props.v.username === 'ssar' ? 'blue' : 'red')};
-  `; //이거는 어쩔 수 없이 내부에 선언 props를 쓰기 위해서, 디자인 동적결정
-
+  //19번 라인에서 위의 styled-component에게 props를 전달해준다.
   return (
     <div>
       <h1>홈 : {number}</h1>
       <button onClick={() => setNumber(number + 1)}>번호증가</button>
-      <StyledDeleteButton v={user} onClick={() => setBoards([])}>
+      <StyledDeleteButton user={user} onClick={() => setBoards([])}>
         전체삭제
       </StyledDeleteButton>
+      <StyledAddButton user={user}>더하기</StyledAddButton>
       {boards.map((board) => (
         <h3>
           제목 : {board.title} 내용:{board.content}
