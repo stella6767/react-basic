608edf737af3591a7f2bdb07e42c05506f923b9e
stella6767 <alsrb9434@gmail.com>
2021-02-03 17:18:56 +0900

    글쓰기,글목록보기 실습(2/3)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { Route } from 'react-router-dom';
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
-import Navigation from './components/Navigation';
 
 function App() {
   //상태
   return (
     <div>
-      <Navigation />
-      <Route path="/" exact={true} component={ListPage} />
-      <Route path="/write" exact={true} component={WritePage} />
+      <ListPage />
     </div>
   );
 }
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -6,14 +6,16 @@ const StyledItemBoxDiv = styled.div`
   padding: 10px;
   height: 100px;
   display: flex;
-  margin: 10px;
+  margin: 20px;
   justify-content: space-between;
   align-items: center;
 `;
 
 const ListPage = () => {
+  const [no, setNo] = useState(6);
+
   const [post, setPost] = useState({
-    id: '',
+    id: 5,
     title: '',
     content: '',
   });
@@ -25,21 +27,14 @@ const ListPage = () => {
     { id: 5, title: '내용5', content: '내용1' },
   ]);
 
-  const handleWrite = () => {
+  const handleWrite = (e) => {
     //listpage의 setPosts에 무엇을 담아야 함?
     //let post = { id: 6, title: '인풋값' };
     //setPosts({ ...posts, id:no });
-    setPosts({ ...posts, post });
-  };
-
-  const handleChangeTitle = (e) => {
-    console.log(e.target.value);
-    setPost({ title: e.target.value });
-  };
-
-  const handleChangeContent = (e) => {
-    console.log(e);
-    setPost({ content: e.target.value });
+    e.preventDefault(); // form태그가 하려는 액션을 중지 시켜야 함.
+    setPosts([...posts, { ...post, id: post.id + 1 }]);
+    //setPosts([...posts, post]);
+    setPost({ ...post, id: post.id + 1 });
   };
 
   const handleForm = (e) => {
@@ -51,10 +46,15 @@ const ListPage = () => {
     console.log(post.title);
     console.log(post.content);
   };
+
+  // 삭제하기
+  const handleDelete = (id) => {
+    setPosts(posts.filter((post) => post.id !== id));
+  };
   return (
     <div>
       <h1>리스트 페이지</h1>
-      <hr />
+
       <form>
         <input
           type="text"
@@ -74,6 +74,7 @@ const ListPage = () => {
           글쓰기
         </button>
       </form>
+      <hr />
       {posts.map((post) => (
         <StyledItemBoxDiv>
           번호:{post.id} / 제목: {post.title} / 내용:{post.content}
